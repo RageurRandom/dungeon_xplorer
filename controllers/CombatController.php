ebb2a47537f23cc1cc032754e504e107c7df280d
berrang232 <berrang232@unicaen.fr>
2024-12-25 09:57:28 +0100

    création d'une fonction getMonster pour créer un combattant avec un id

--- controllers/CombatController.php
+++ controllers/CombatController.php
@@ -4,6 +4,39 @@ class CombatController{
         session_start();
         require_once 'views/combat.php';
     }
+
+    /**
+     * créé une instance de Combattant en cherchant un monstre dans la BDD
+     * @param int monster_id l'id du monstre à chercher
+     * @return Combattant monstre trouvé ou NULL si aucun monstre avec cet ID n'a été trouvé
+     */
+    public function getMonster($monster_id){
+        $DB = DataBase::getInstance();
+
+        try {
+            $querry = "SELECT monster_name, monster_HP, monster_mana, monster_strength, monster_initiative FROM monster
+            WHERE monster_id = ?";
+
+            $statement = $DB->prepare_statement($querry);
+
+            $statement->bindParam(1, $monster_id);
+            $statement->execute();
+            $results = $statement->fetchAll();
+        } catch (Exception $e) {
+            die("Erreur getMonster : " . $e->getMessage());
+        }
+
+        if(count($results) > 0){
+            $row = $results[0];
+
+            $res = new Combattant($row["monster_name"], $row["monster_HP"], $row["monster_HP"], $row["monster_mana"], $row["monster_mana"], $row["monster_initiative"], $row["monster_strength"]);
+            
+            return $res;
+        } else {
+            die(print_r($results));
+            return NULL;
+        }
+    }
 }
 
 ?>
--- views/combat.php
+++ views/combat.php
@@ -6,26 +6,30 @@
     <title>Combat</title>
 </head>
 <body>
-<?php
     
-    $_SESSION["monster"] = new Combattant("cap chat", 5, 5, 3, 3, 2, 2); //pour les tests
+<?php
+    $controller = new CombatController();
+    $monster_test = $controller->getMonster(2); //pour les tests
     
     if(!isset($_SESSION["hero"])){
         $_SESSION["hero"] = new Warrior(0, 1, 1, "Sir Alain Juppé", 7, 10, 12, 0, 0, 5, 3, 0); //tj pour les tests
     }
 
-    if(!isset($_SESSION["monster"])){
+    if(!isset($monster_test)){
         //problème
-        die();
+        die("Erreur : Ce monstre n'existe pas dans la base de données");
     }
 
-    echo "<div><h2>" . $_SESSION["monster"]->getName() . "</h2>"
-    . "<div>" . $_SESSION["monster"]->getCurrentHP() . "/" . $_SESSION["monster"]->getMaxHP() . " HP </div>"
-    . "<div>" . $_SESSION["monster"]->getCurrentMana() . "/" . $_SESSION["monster"]->getMaxMana() . " Mana </div> </div>";
 
-    echo "<div><h2>" . $_SESSION["hero"]->getName() . "</h2>"
-    . "<div>" . $_SESSION["hero"]->getCurrentHP() . "/" . $_SESSION["hero"]->getMaxHP() . " HP </div>"
-    . "<div>" . $_SESSION["hero"]->getCurrentMana() . "/" . $_SESSION["hero"]->getMaxMana() . " Mana </div> </div>";
+    
+        echo "<div><h2>" . $monster_test->getName() . "</h2>"
+        . "<div>" . $monster_test->getCurrentHP() . "/" . $monster_test->getMaxHP() . " HP </div>"
+        . "<div>" . $monster_test->getCurrentMana() . "/" . $monster_test->getMaxMana() . " Mana </div> </div>";
+
+        echo "<div><h2>" . $_SESSION["hero"]->getName() . "</h2>"
+        . "<div>" . $_SESSION["hero"]->getCurrentHP() . "/" . $_SESSION["hero"]->getMaxHP() . " HP </div>"
+        . "<div>" . $_SESSION["hero"]->getCurrentMana() . "/" . $_SESSION["hero"]->getMaxMana() . " Mana </div> </div>";
+    
 ?>
 <div>Votre tour : </div>
 <button>Attaque</button>
