555e98a6fb5ff2db127eb9552c954eb5eaf11d95
LukAtHim <153590660+LukAtHim@users.noreply.github.com>
2024-12-25 01:45:20 +0100

    css : creation de compte

--- views/connexion.php
+++ views/connexion.php
@@ -5,8 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- <link href="assets/css/style.css" rel="stylesheet" /> -->
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -34,7 +32,7 @@
       <label for="userPassword" class="sr-only">Password</label>
       <input type="password" id="userPassword" class="form-control mb-3" placeholder="Password" name="userPassword" required>
 
-      <button class="btn btn-primary btn-lg btn-light mb-3 " type="submit">SE CONNECTER</button>
+      <button class="btn btn-primary btn-lg btn-light mb-3" type="submit">SE CONNECTER</button>
       <a class="btn btn-link" href="creationCompte">créer un compte</a>
 
     </form>
