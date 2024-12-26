a2444b9dea6703a68792e7df82aebe7435f70c15
LukAtHim <153590660+LukAtHim@users.noreply.github.com>
2024-12-26 17:40:00 +0100

    css choix personnage

--- views/personnage/creationPersonnage.php
+++ views/personnage/creationPersonnage.php
@@ -3,29 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="assets/css/choixPersonnage.css" rel="stylesheet" />
+
+    <!-- <link href="assets/css/choixPersonnage.css" rel="stylesheet" /> -->
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous" defer></script>
+
+    <!-- Custom CSS -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    
     <title>création héro</title>
 </head>
 <body>
     <form action="creationHero" method = "post">
 
-    <header>
+    <div class="container text-center mt-5">
         <h1> Choix du héro</h1>
         <div class="wrapper">
             <div class="box"><input type="text" placeholder="nom de l'héro" id="heroName" name="heroName" required/></div>
         </div>
-    </header>
 
-        <section class="GUERRIER">
-            <button class="btn" name="heroClass" type="submit" value="GUERRIER">GUERRIER</button>
-		</section>
+        <div class="row mt-4">
+
+            <div class="col-md-4 col-sm-12">
+                <div class="character">
+                    <img src="assets/images/bw_guerrier.jpeg" alt="guerrier" class="img-fluid">
+                    <p>GUERRIER</p>
+                    <button class="btn" name="heroClass" type="submit" value="GUERRIER">GUERRIER</button>
+
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-12">
+                <div class="character">
+                    <img src="assets/images/bw_mage.jpeg" alt="guerrier" class="img-fluid">
+                    <p>MAGICIEN</p>
+                    <button class="btn" name="heroClass" type="submit" value="MAGICIEN">MAGICIEN</button>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-12">
+                <div class="character">
+                    <img src="assets/images/bw_voleur.jpeg" alt="guerrier" class="img-fluid">
+                    <p>VOLEUR</p>
+                    <button class="btn" name="heroClass" type="submit" value="VOLEUR">VOLEUR</button>
+                </div>
+            </div>
+                
 		  
 		<section class="MAGICIEN">
-            <button class="btn" name="heroClass" type="submit" value="MAGICIEN">MAGICIEN</button>
 		</section>
 		  
 		<section class="VOLEUR">
-            <button class="btn" name="heroClass" type="submit" value="VOLEUR">VOLEUR</button>
 		</section>
 
     </form>
