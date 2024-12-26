c5a1d865ef26ce369fc28c9ea6e3b9b06a9eb63d
mathis berot <berotmathis@gmail.com>
2024-12-26 17:39:07 +0100

    ajout de css pour les chapitres

--- views/chapitre.php
+++ views/chapitre.php
@@ -3,31 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/styleChapitre.css" rel="stylesheet">
     <title>Accueil</title>
 </head>
 <body>
-<h1> Chapitre <?php echo $hero->getChapter(); ?> </h1>
+    <h1> Chapitre <?php echo $hero->getChapter(); ?> </h1>
     
-    <div>
-        <p>
-            <?php
-                //On affiche le hero
-                $this->printHero($hero); 
-            ?>
-        </p>
+    <div class="content">
+        <div class="hero-section">
+            <p>
+                <?php
+                    // On affiche le hero
+                    $this->printHero($hero); 
+                ?>
+            </p>
+        </div>
+
+        <div class="chapter-content"> 
+            <p>
+                <?php echo $chapterInfos[0]["chapter_content"] ?>
+            </p> 
+            <ul class="buttons">
+                <?php
+                    $this->printLinks($links); 
+                ?>
+            </ul>
+        </div>
+
+        
     </div>
-
-    <div> 
-        <p>
-            <?php echo $chapterInfos[0]["chapter_content"] ?>
-        </p> 
-    </div>
-
-    <ul>
-        <?php
-            $this->printLinks($links); 
-        ?>
-    </ul>
-
 </body>
 </html>
